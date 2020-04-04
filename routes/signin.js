12f9ca462619f5223d31f8a5742842672be72545
dmytrolutsyk <49659495+dmytrolutsyk@users.noreply.github.com>
2020-04-04 10:26:38 +0200

    update route signin.js

--- routes/signin.js
+++ routes/signin.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var jwtUtils = require('../utils/jwt.utils');
+var bcrypt = require('bcrypt');
 var router = express.Router();
 const {MongoClient} = require('../config');
 const {MONGODB_URI} = require('../config');
@@ -8,6 +10,9 @@ router.post('/', async function(req, res){
     //Params
     var UserName = req.body.UserName;
     var PassWord = req.body.PassWord;
+    var UserFound = null;
+    var UserPassWord = null;
+    var UserId = null;
   
     //connecion bdd
     const client = new MongoClient(MONGODB_URI, {userNewUrlParser: true});
@@ -15,35 +20,44 @@ router.post('/', async function(req, res){
     const db = client.db(dbName);
     const col = db.collection('users');
     console.log('Connected\n');
-  
+
     if (UserName == null || PassWord == null){
         return res.status(400).json({ 'error' : 'missing parameters' });
+    }else if(PassWord.length <= 3) {
+        return res.status(400).json({'error': 'Le mot de passe doit contenir au moins 4 caractères'});
+    }else if(UserName.length <= 2 || UserName.length >= 21 ) {
+        return res.status(400).json({'error': 'Votre identifiant doit contenir entre 2 et 20 caractères'});
     }
-  
-    await col.findOne({
-        where: { UserName: UserName}
-    })
-    .then(function(userFound) {
-        if (userFound){
-            bcrypt.compare(PassWord, userFound.PassWord, function(errBycrypt, resBycrypt) {
-                if (resBycrypt) {
-                    return res.status(200).json({
-                        'UserId': userFound.id,
-                        'token': jwtUtils.generateTokenForUser(userFound)
-                    });
-                } else {
-                    return res.status(403).json({ "error": "invalid password"});
-                }
-            });
-  
-        } else {
-            return res.status(404).json({ 'error': 'user not exit in DB' });
+
+    let data = await col.find({}).toArray();
+    console.log('req.body.UserName:', req.body.UserName)
+    if (data.some(data => data.UserName === req.body.UserName)) {
+        console.log("ok")
+        data.forEach(element => {
+            if(element.UserName === req.body.UserName){
+               console.log("zebi")
+               userFound = req.body.UserName
+            //    UserFound = data.UserName;
+               UserPassWord = element.PassWord;
+               UserId = element._id;
+               console.log(UserFound, UserPassWord, UserId)
+            }
+        });
+        bcrypt.compare(PassWord, UserPassWord, function(errBycrypt, resBycrypt) {
+            if (resBycrypt) {
+                return res.status(200).json({
+                    'UserId': UserId,
+                    'token': jwtUtils.generateTokenForUser(UserName)
+                });
+            } else {
+                return res.status(403).json({ "error": "invalid password"});
+            }
+        });
+    }else {
+            return res.status(403).json({ 'error': 'Cet identifiant est inconnu' });
         }
-    })
-    .catch(function(err){
-        return res.status(500).json({ 'error': 'unable to verify user' });
     });
-  });
+
   
   
   
